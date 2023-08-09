5cb0125e50e1c0f43dd90686298ac13a5282711b
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-08-09 21:51:51 +0800

    create landing page

---
+++ landing/app/page.tsx
@@ -0,0 +1,38 @@
+import LandingDescription from '@/components/LandingDescription'
+
+export default function Home() {
+  return (
+    <div className='flex flex-col justify-center items-center min-h-screen bg-black text-white'>
+        <div className='text-center max-w-[1000px]'>
+            <LandingDescription />
+
+            <div className='flex flex-row items-center justify-center gap-16'>
+                <a href={'/dashboard'}>
+                  <button className='bg-white hover:bg-red-500 px-5 py-3 rounded-lg'>
+                      <h1 className='text-black font-bold'>
+                        Launch on Optimism Goerli
+                      </h1>
+                  </button>
+                </a>
+                <a href={'/dashboard'}>
+                  <button className='bg-white hover:bg-blue-500 px-5 py-3 rounded-lg'>
+                      <h1 className='text-black font-bold'>
+                        Launch on Base Goerli
+                      </h1>
+                  </button>
+                </a>
+
+                <a href={'/dashboard'}>
+                  <button className='bg-white hover:bg-purple-500 hover:animate-text px-5 py-3 rounded-lg'>
+                      <h1 className='text-black font-bold'>
+                        Launch on Zora
+                      </h1>
+                  </button>
+                </a>
+            </div>
+
+            <p className='text-white mt-16'>Note : Ethereum attestation services (eas) are only avaliable on Optimism and Base</p>
+        </div>
+    </div>
+  )
+}
