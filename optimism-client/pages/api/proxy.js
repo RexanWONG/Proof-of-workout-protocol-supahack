b2c35f91eabfc7945016cddb6db121e194cdaa67
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-08-11 20:00:56 +0800

    update api url

--- optimism-client/pages/api/proxy.js
+++ optimism-client/pages/api/proxy.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   try {
-    const url = "https://7a8e-173-244-62-29.ngrok-free.app/user-activities";
+    const url = "https://fef6-173-244-62-39.ngrok-free.app/user-activities";
     const params = {
       params: req.query
     };
