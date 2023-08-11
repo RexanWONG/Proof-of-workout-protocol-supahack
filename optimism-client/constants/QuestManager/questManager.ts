9c17ec4c307bd4d2496e67afa72ed02d822f342a
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-08-11 12:51:51 +0800

    update to new contract

--- optimism-client/constants/QuestManager/questManager.ts
+++ optimism-client/constants/QuestManager/questManager.ts
@@ -1,5 +1,5 @@
 import abi_data from './QuestManager.json'; 
 
 export const abi = abi_data.abi;
-export const contractAddress = '0x78584CE63aeCd8943D8A4119e679902fdA0B8C2d';
+export const contractAddress = '0x4B704d0f4AF342086e585e4F3AD6B263fD409662';
 
